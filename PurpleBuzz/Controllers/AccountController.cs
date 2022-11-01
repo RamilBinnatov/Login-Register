5746e865e4b7c141c4f29d340bc7636eea6a523a
RamilBinnatov <ramilrb@code.edu.az>
2022-11-02 00:59:19 +0400

    Login-Register

---
+++ PurpleBuzz/Controllers/AccountController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PurpleBuzz.Models;
+using PurpleBuzz.ViewModels.AccountViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PurpleBuzz.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(registerVM);
+            }
+
+            AppUser user = new AppUser
+            {
+                Fullname = registerVM.FullName,
+                UserName = registerVM.Username,
+                Email = registerVM.Email
+            };
+
+            IdentityResult result = await _userManager.CreateAsync(user, registerVM.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+                return View(registerVM);
+            }
+
+
+            return RedirectToAction(nameof(Login));
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (!ModelState.IsValid) return View(loginVM);
+
+            AppUser user = await _userManager.FindByEmailAsync(loginVM.UsernameOrEmail);
+
+            if (user is null)
+            {
+                user = await _userManager.FindByNameAsync(loginVM.UsernameOrEmail);
+            }
+
+            if (user is null)
+            {
+                ModelState.AddModelError("", "Email or Password is wrong");
+                return View(loginVM);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Email or password wrong");
+                return View(loginVM);
+            }
+
+            return RedirectToAction("index", "Home");
+        }
+
+
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
+    }
+}
---
+++ PurpleBuzz/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PurpleBuzz.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PurpleBuzz.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<slider> Sliders { get; set; }
+
+        public DbSet<Work> Works { get; set; }
+    }
+}
